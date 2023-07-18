f47b94b9d690f7d2f2b8786b06df0ef7ecddf9dc
rukaiya20 <rukaiyajahantajin239@gmail.com>
2023-07-18 13:07:00 +0600

    card details modal added

--- index.js
+++ index.js
@@ -19,17 +19,17 @@ const showInformation = (data, dataLimit) => {
   data.forEach(singleInfo => {
       // console.log(singleInfo)
       const card = document.createElement('div');
-      card.classList.add('card', 'w-full', 'bg-base-100', 'shadow-sm', 'border');
+      card.classList.add('card', 'w-full', 'bg-base-100', 'shadow-sm', 'border','border-gray-200');
       card.innerHTML = `
       <figure class="p-3 ">
           <img src="${singleInfo.image}"
-          alt="Shoes"
+          alt="image"
           class="rounded-xl" style="height:230px" />
       </figure>
       <div class="card-body px-3">
           <h2 class="card-title font-bold">
           Features</h2>
-          <ol id="${singleInfo.id}">
+          <ol class="text-gray-600" id="${singleInfo.id}">
           </ol>
           <hr class="border-1">
           <div
@@ -37,13 +37,12 @@ const showInformation = (data, dataLimit) => {
               <div>
                   <h2
                       class="text-1xl font-bold mb-2">${singleInfo.name}</h2>
-                  <p><i
-                          class="fa-solid fa-calendar-days "></i>
+                  <p class="text-neutral-500"><i class="fa-solid fa-calendar-days "></i>
                       ${singleInfo.published_in}</p>
               </div>
               <div>
-              <label for="modalBody" onclick="loadDetailsInfo(${singleInfo.id})"><i
-              class="fa-solid fa-arrow-right text-red-500 cursor-pointer"></i></label> 
+              <label for="modalBody" onclick="loadDetailsInfo(${singleInfo.id})">
+              <i class="fa-solid fa-arrow-right text-red-500 cursor-pointer mt-9"></i></label> 
               </div>
           </div>
       </div>
@@ -62,7 +61,15 @@ const showInformation = (data, dataLimit) => {
 }
 
 //spinner part
-
+const showSpinner = (isLoading) => {
+  const spinner = document.getElementById('spinner')
+  if (isLoading) {
+      spinner.classList.remove('hidden')
+  }
+  else {
+      spinner.classList.add('hidden')
+  }
+}
 
 fetchAllInformation(6)
 
@@ -72,3 +79,73 @@ const showAllData = () => {
 }
 
 //load item details data
+const loadDetailsInfo = async (itemId) => {
+  let id = 0;
+  if (itemId < 10) {
+      id = `0${itemId}`
+  }
+  else {
+      id = itemId
+  }
+  const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`
+  const res = await fetch(url);
+  const data = await res.json();
+  cardDetails(data.data)
+}
+
+
+
+  //accuracy
+  const accuracy = document.getElementById('accuracy')
+  if (data.accuracy.score === null) {
+      accuracy.innerHTML = ''
+  }
+  else {
+      accuracy.innerHTML = `
+      <p class="bg-red-500 text-white font-bold py-2 px-4 rounded-lg"><span>
+      ${data.accuracy.score * 100}</span> % accuracy</p>
+      `
+  }
+  //features part
+  const featuresInfo = document.getElementById('features-infoContainer');
+  if (data.features == {}) {
+      const p = document.createElement('p');
+      p.innerText = 'No data Found';
+      featuresInfo.appendChild(p)
+  }
+  else {
+      for (const item in data.features) {
+          const li = document.createElement('li')
+          li.innerText = data.features[`${item}`].feature_name;
+          featuresInfo.appendChild(li)
+      }
+  }
+  //Integrations
+  const integrationInfo = document.getElementById('integration-infoContainer')
+  if (data.integrations === [] || data.integrations === null) {
+      const p = document.createElement('p');
+      p.innerText = 'No data Found';
+      integrationInfo.appendChild(p)
+  }
+  else {
+      data.integrations.forEach(integrations => {
+          const li = document.createElement('li');
+          li.innerText = integrations;
+          integrationInfo.appendChild(li)
+      })
+  }
+};
+
+//sort by data section
+
+const sortByData = async () => {
+  const res = await fetch('https://openapi.programming-hero.com/api/ai/tools');
+  const data = await res.json();
+  sortData(data.data.tools)
+}
+const sortData = (data) => {
+  const newArr = data.sort(function (a, b) {
+      return new Date(a.published_in) - new Date(b.published_in);
+  });
+  showInformation(newArr)
+}
